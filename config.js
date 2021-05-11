c495ca3b9b4c9166a334b4ddd2627e9252cccc65
Boussinesq-Li <53210145+Boussinesq-Li@users.noreply.github.com>
2021-05-11 19:50:27 +0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+// by BingLi 2021年05月11日
+var config = {
+    texts: [
+        "祝龙真生日快乐！",    
+    ],
+            desc: {
+        turn_on: "开始",
+        play: "音乐",
+        bannar_coming: "颜色",
+        balloons_flying: "好像少点东西",
+        cake_fadein: "蛋糕？",
+        light_candle: "蜡烛？",
+        wish_message: "生日快乐",
+        story: "A MESSAGE FOR YOU",
+    }
+};
